03b9699617ee27f58896047bab602f67da477c0d
Chak <444chak@gmail.com>
2023-05-16 09:49:26 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+index.php
+</!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+<ul>
+		<li><a href="phpinfo.php">PHP.INFO</a></li>
+		<li><a href="sqlrequest.php">Requête sur la base de donnée ></a></li>
+		<li><a href="script.php">Script PHP unique à notre de groupe</a></li>
+	
+</ul>
+</body>
+</html>
