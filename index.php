ff03c146ff6d4898574d1c89b99b2c8bcabd6f6b
444chak <444chak@gmail.com>
2023-05-30 14:35:31 +0000

    add table in index.php and style that goes with

--- index.php
+++ index.php
@@ -3,15 +3,31 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="style/style.css" rel="stylesheet" type="text/css" />
+	<link href="style.css" rel="stylesheet" type="text/css" />
 	<title>SAé 2.03 - OUALI BARKER</title>
 </head>
 <body>
-<ul>
-		<li><a href="phpinfo.php">PHP.INFO</a></li>
-		<li><a href="sqlrequest.php">Requête sur la base de donnée ></a></li>
-		<li><a href="script.php">Script PHP unique à notre de groupe</a></li>
-	
-</ul>
+	<table>
+	<tr>
+		<th>Titre</th>
+		<th>Description</th>
+		<th>Lien</th>
+	</tr>
+	<tr>
+		<td>phpinfo</td>
+		<td>Afficher les informations relatives au PHP</td>
+		<td><a href="phpinfo.php"><button>Afficher</button></a></td>
+	</tr>
+	<tr>
+		<td>Requête SQL</td>
+		<td>Effectue un script qui permet d'ajouter un commentaire</td>
+		<td><a href="sqlrequest.php"><button>Afficher</button></a></td>
+	</tr>
+	<tr>
+		<td>Script PHP</td>
+		<td>Petite application permettant d'envoyer des images et de les modifier avec le module Imagick.</td>
+		<td><a href="script.php"><button>Afficher</button></a></td>
+	</tr>
+	</table>
 </body>
 </html>
