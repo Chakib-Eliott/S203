e08bb7d0fca1efd83ca0f9cf5562525b3ec706d1
Eliott-B <eliottb.info@gmail.com>
2023-05-30 15:33:15 +0200

    Add form

--- sqlrequest.php
+++ sqlrequest.php
@@ -1,1 +1,23 @@
-
+<!DOCTYPE html>
+<html lang='fr'>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Avis S203</title>
+        <meta name="description" content="Page d'avis sur la SAé S203">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="asset/style.css">
+    </head>
+    <body>
+        <header>
+            <h1>Avis S203</h1>
+        </header>
+        <form action="sqlrequest.php" method="post">
+            <label for="pseudo">Votre pseudo :</label>
+            <input type="text" name="pseudo"/><br><br>
+            <label for="comment">Votre commentaire :</label><br>
+            <textarea name="comment" rows="5" cols="33"> </textarea><br><br>
+            <input type="submit" value="Poster">
+        </form>
+    </body>
+</html>
